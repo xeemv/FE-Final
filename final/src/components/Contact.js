44d03bcc76fbf1185e304a954c6d4fec6ecd76ab
Xeemv <xeeyang08@gmail.com>
2022-08-14 20:06:21 -0500

    finished

--- final/src/components/Contact.js
+++ final/src/components/Contact.js
@@ -1,48 +1,44 @@
 import React from "react";
 import { useState } from "react";
+import emailjs from "emailjs-com";
 
 const FORM_ENDPOINT = ""; // TODO - fill on the later step
 
 const Contact = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100);
-  };
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  if (submitted) {
-    return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
-      </>
-    );
-  }
+
+    emailjs.sendForm(
+      "service_0h2iuao", 
+      "template_43igvj6", 
+      e.target, 
+      "hTad8af4VNgXRqYZj"
+      ).then(res=>{
+        console.log(res);
+    }).catch(err=> console.log(err));
+  } 
 
   return (
     <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit}
-      method="POST"
-      target="_blank"
+      onSubmit={sendEmail}
     >
       <div className="mb-3 pt-0">
         <input
           type="text"
           placeholder="Your name"
           name="name"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="form-control"
           required
         />
       </div>
 
       <div className="mb-3 pt-0">
         <input
-          type="email"
+          type="user_email"
           placeholder="Email"
-          name="email"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          name="user_email"
+          className="form-control"
           required
         />
       </div>
@@ -51,14 +47,14 @@ const Contact = () => {
         <textarea
           placeholder="Your message"
           name="message"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="form-control"
           required
         />
       </div>
 
       <div className="mb-3 pt-0">
         <button
-          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          className="form-control"
           type="submit"
         >
           {" "}
@@ -71,3 +67,6 @@ const Contact = () => {
 
 // export default ContactForm;
 export default Contact;
+
+
+// bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150
---
+++ final/src/components/navbar-pages/Mailer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mailer = () => {
+  return (
+    <div>Mailer</div>
+  )
+}
+
+export default Mailer;
